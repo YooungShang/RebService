e0b7dc1a642506407cf661f98ae7ba75014fafbc
YaHuOnWork <122421136+YaHuOnWork@users.noreply.github.com>
2023-11-15 22:44:10 +0800

    learning...

--- reb_service/http/src/httprequest.rs
+++ reb_service/http/src/httprequest.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::io::Read;
+
 #[derive(Debug, PartialEq)]
 pub enum Method{
     Get,
@@ -34,6 +37,78 @@ impl From<&str> for Version{
 #[derive(Debug, PartialEq)]
 pub enum Resource{
     Path(String),
+}
+
+#[derive(Debug)]
+pub struct HttpRequest{
+    pub method: Method,
+    pub version: Version,
+    pub resource: Resource,
+    pub headers: HashMap<String, String>,
+    pub msg_body: String,
+}
+
+impl From<String> for HttpRequest{
+    fn from(value: String) -> Self {
+        let mut parsed_method = Method::Uninitialized;
+        let mut parsed_version = Version::V1_1;
+        let mut parsed_resource = Resource::Path("".to_string());
+        let mut parsed_headers = HashMap::new();
+        let mut parsed_msg_body = "";
+
+        for line in value.lines() {
+            if line.contains("HTTP") {
+                let (method, resource, version) = process_req_line(line);
+                parsed_method = method;
+                parsed_resource = resource;
+                parsed_version = version;
+            } else if line.contains(":") {
+                let (key, value) = process_header_line(line);
+                parsed_headers.insert(key, value);
+            } else if line.len() == 0 {
+
+            }else {
+                parsed_msg_body = line;
+            }
+        }
+
+        HttpRequest{
+            method: parsed_method,
+            version: parsed_version,
+            resource: parsed_resource,
+            headers: parsed_headers,
+            msg_body: parsed_msg_body.to_string(),
+        }
+    }
+}
+
+fn process_req_line(s: &str) -> (Method, Resource, Version){
+    let mut words = s.split_whitespace();
+    let method = words.next().unwrap();
+    let resource = words.next().unwrap();
+    let version = words.next().unwrap();
+
+    (
+        method.into(),
+        Resource::Path(resource.to_string()),
+        version.into(),
+    )
+}
+
+fn process_header_line(s: &str) -> (String, String){
+    let mut header_items = s.split(":");
+    let mut key = String::from("");
+    let mut value = String::from("");
+
+    if let Some(k) = header_items.next() {
+        key = k.to_string();
+    }
+
+    if let Some(v) = header_items.next(){
+        value = v.to_string();
+    }
+
+    (key, value)
 }
 #[cfg(test)]
 mod tests{
